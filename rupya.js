3e5bc11480aa1df428b6536bae843d78b2fb4fc9
Rupya1002 <rupya13017.24@kgpian.iitkgp.ac.in>
2025-03-31 14:05:57 +0530

    minor change

--- rupya.js
+++ rupya.js
@@ -11,16 +11,11 @@ function addToWishlist(i) {
 
   wishlist.push(book);
   localStorage.setItem('wishlist', JSON.stringify(wishlist));
-  // Addingbooksinwishlist(book.category, document.getElementById('wishlist-container'));
 
-  alert(`${book.title} has been added to your wishlist!`);
 }
 
-// let bookmark = document.getElementsByClassName('bookmark');
-// bookmark.forEach(element => {
-//   element.addEventListener('click', () => {
-//     i=element.parentElement.parentElement.id;
-//     console.log(i)
-//     addToWishlist(i);
-//   })  
-// });
+
+function toggleWishlist(element) {
+  console.log('hello');
+  element.classList.toggle('active');
+}
